be4e0c8370a5283522584e8fc4127efe713846ff
LeeChiaHao <89331388+LeeCH928@users.noreply.github.com>
2022-03-17 13:08:40 +0800

    adjust file strucutre (del redundant folder)

--- E_Voting/migrations/2_deploy_contracts.js
+++
@@ -1,6 +0,0 @@
-const Voters = artifacts.require("Voters");
-const Elections = artifacts.require("Elections");
-module.exports = async function (deployer) {
-    await deployer.deploy(Voters);
-    await deployer.deploy(Elections, Voters.address);
-};
